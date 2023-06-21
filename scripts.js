7ed4a9f960b128c4157b9fef4b5c484c8c630665
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2023-06-21 11:04:18 -0400

    change rank names to all be strings, use strict equalizers when comparing ranks in all functions

--- scripts.js
+++ scripts.js
@@ -1,58 +1,58 @@
 const deck = [
-    {suit: 'spades', rank:2},
-    {suit: 'spades', rank:3},
-    {suit: 'spades', rank:4},
-    {suit: 'spades', rank:5},
-    {suit: 'spades', rank:6},
-    {suit: 'spades', rank:7},
-    {suit: 'spades', rank:8},
-    {suit: 'spades', rank:9},
+    {suit: 'spades', rank:"2"},
+    {suit: 'spades', rank:"3"},
+    {suit: 'spades', rank:"4"},
+    {suit: 'spades', rank:"5"},
+    {suit: 'spades', rank:"6"},
+    {suit: 'spades', rank:"7"},
+    {suit: 'spades', rank:"8"},
+    {suit: 'spades', rank:"9"},
     {suit: 'spades', rank:"t"},
     {suit: 'spades', rank:"j"},
     {suit: 'spades', rank:"q"},
     {suit: 'spades', rank:"k"},
     {suit: 'spades', rank:"a"},
-    {suit: 'hearts', rank:2},
-    {suit: 'hearts', rank:3},
-    {suit: 'hearts', rank:4},
-    {suit: 'hearts', rank:5},
-    {suit: 'hearts', rank:6},
-    {suit: 'hearts', rank:7},
-    {suit: 'hearts', rank:8},
-    {suit: 'hearts', rank:9},
+    {suit: 'hearts', rank:"2"},
+    {suit: 'hearts', rank:"3"},
+    {suit: 'hearts', rank:"4"},
+    {suit: 'hearts', rank:"5"},
+    {suit: 'hearts', rank:"6"},
+    {suit: 'hearts', rank:"7"},
+    {suit: 'hearts', rank:"8"},
+    {suit: 'hearts', rank:"9"},
     {suit: 'hearts', rank:"t"},
     {suit: 'hearts', rank:"j"},
     {suit: 'hearts', rank:"q"},
     {suit: 'hearts', rank:"k"},
     {suit: 'hearts', rank:"a"},
-    {suit: 'clubs', rank:2},
-    {suit: 'clubs', rank:3},
-    {suit: 'clubs', rank:4},
-    {suit: 'clubs', rank:5},
-    {suit: 'clubs', rank:6},
-    {suit: 'clubs', rank:7},
-    {suit: 'clubs', rank:8},
-    {suit: 'clubs', rank:9},
+    {suit: 'clubs', rank:"2"},
+    {suit: 'clubs', rank:"3"},
+    {suit: 'clubs', rank:"4"},
+    {suit: 'clubs', rank:"5"},
+    {suit: 'clubs', rank:"6"},
+    {suit: 'clubs', rank:"7"},
+    {suit: 'clubs', rank:"8"},
+    {suit: 'clubs', rank:"9"},
     {suit: 'clubs', rank:"t"},
     {suit: 'clubs', rank:"j"},
     {suit: 'clubs', rank:"q"},
     {suit: 'clubs', rank:"k"},
     {suit: 'clubs', rank:"a"},
-    {suit: 'diamonds', rank:2},
-    {suit: 'diamonds', rank:3},
-    {suit: 'diamonds', rank:4},
-    {suit: 'diamonds', rank:5},
-    {suit: 'diamonds', rank:6},
-    {suit: 'diamonds', rank:7},
-    {suit: 'diamonds', rank:8},
-    {suit: 'diamonds', rank:9},
+    {suit: 'diamonds', rank:"2"},
+    {suit: 'diamonds', rank:"3"},
+    {suit: 'diamonds', rank:"4"},
+    {suit: 'diamonds', rank:"5"},
+    {suit: 'diamonds', rank:"6"},
+    {suit: 'diamonds', rank:"7"},
+    {suit: 'diamonds', rank:"8"},
+    {suit: 'diamonds', rank:"9"},
     {suit: 'diamonds', rank:"t"},
     {suit: 'diamonds', rank:"j"},
     {suit: 'diamonds', rank:"q"},
     {suit: 'diamonds', rank:"k"},
     {suit: 'diamonds', rank:"a"}
 ]
-const askForCard = document.getElementById("ask-bot")
+const doesBotHaveAny = document.getElementById("do-you-have")
 const botScoreBoard = document.getElementById("bot-score")
 const cardImages = Array.from(document.querySelectorAll(".playing-card"))
 const goFish = document.getElementById("go-fish")
@@ -65,7 +65,7 @@ const myTurn = document.getElementById("player-turn")
 const okStart = document.getElementById("ok-start")
 const playerScoreBoard = document.getElementById("player-score")
 const rankButtons = Array.from(document.querySelectorAll(".rank-button"))
-const ranks = [2, 3, 4, 5, 6, 7, 8, 9, "t", "j", "q", "k", "a"]
+const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "t", "j", "q", "k", "a"]
 const scoreBoard = document.getElementById("score")
 const startButton = document.getElementById("start-button")
 const scoreHider = document.getElementById("score-button")
@@ -105,7 +105,7 @@ scoreHider.addEventListener("click", function() {
 function activateRankButtons() {
     rankButtons.forEach(button => {
         button.addEventListener("click", function() {
-            askBot(button.id.slice(4))
+            askBot(button.id)
         })
     })
 }
@@ -151,7 +151,7 @@ yourTurn.addEventListener("click", function() {
 
 function playerTurn() {
     message.textContent = "Ask me for a card rank you've got!"
-    toggleVisibility(askForCard)
+    toggleVisibility(doesBotHaveAny)
 }
 
 myTurn.addEventListener("click", function() {
@@ -192,17 +192,17 @@ okStart.addEventListener("click", function() {
 
 /* Displays list of ranks you can ask for */
 
-askForCard.addEventListener ("click", function() {
-    toggleVisibility(askForCard)
+function doesBotHave() {
+    toggleVisibility(doesBotHaveAny)
     rankButtons.forEach(button => {
         playerHand.forEach(card => {
-            const checkRank = button.id.slice(4)
-            if(checkRank==card.rank && !button.classList.contains("visible")) {
+            const checkRank = button.id
+            if(checkRank===card.rank && !button.classList.contains("visible")) {
             toggleVisibility(button)
             } 
         })
     })
-})
+}
 
 /* Draws random cards into player's/bot's hands */
 
@@ -271,7 +271,7 @@ function checkEmptyPlayerHand() {
 function askBot(askRank) {
     let successful = "no"
     botHand.forEach (card => {
-        if (card.rank == askRank) {
+        if (card.rank === askRank) {
             playerHand.unshift(card)
             const takenCardImage = document.getElementById(`${card.suit}`+`${card.rank}`)
             toggleVisibility(takenCardImage)
@@ -295,7 +295,7 @@ function askBot(askRank) {
         message.textContent = "Darn, I do! Here you go, you rapscallion..."
         toggleVisibility(yourTurn)
         playerPointCheck()
-        botHand = botHand.filter(card => card.rank!=askRank)
+        botHand = botHand.filter(card => card.rank!==askRank)
         handCheck()
     }
 }
@@ -333,7 +333,7 @@ function botAsk() {
         convertedRank=askRank
         rankConvert(askRank)
         message.textContent = `I'll just go ahead and take any ${convertedRank}s you have.`
-        playerHand = playerHand.filter(card => card.rank!=askRank)
+        playerHand = playerHand.filter(card => card.rank!==askRank)
     }
     if (successful === "no") {
         if (playingDeck.length===0) {
@@ -372,14 +372,14 @@ function playerPoint() {
 
 function playerPointCheck() {
     ranks.forEach(targetRank => {
-        const checkingObject = playerHand.filter(card => card.rank==targetRank)
+        const checkingObject = playerHand.filter(card => card.rank===targetRank)
         if (checkingObject.length === 4) {
             cardImages.forEach(image => {
-                if (image.id.slice(-1)==targetRank && image.classList.contains("visible")) {
+                if (image.id===targetRank && image.classList.contains("visible")) {
                     toggleVisibility(image)
                 }
             })
-            playerHand = playerHand.filter(card => card.rank!=targetRank)
+            playerHand = playerHand.filter(card => card.rank!==targetRank)
             playerPoint()
             if(!modalMessage.textContent.includes(`that's game`)) {
                 playerPointModal(targetRank)
@@ -405,9 +405,9 @@ function botPoint() {
 
 function botPointCheck() {
     ranks.forEach(targetRank => {
-        const checkingObject = botHand.filter(card => card.rank==targetRank)
+        const checkingObject = botHand.filter(card => card.rank===targetRank)
         if (checkingObject.length === 4) {
-            botHand = botHand.filter(card => card.rank!=targetRank)
+            botHand = botHand.filter(card => card.rank!==targetRank)
             botPoint()
             if(!modalMessage.textContent.includes(`that's game`)) {
                 botPointModal(targetRank)
